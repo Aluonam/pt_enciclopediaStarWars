42f4de2f0eeedc45dfa465dc507784b7882390fa
Aluon <paulacanoroman@gmail.com>
2023-09-16 22:07:18 +0200

    consumo API

--- pt-enciclopedia/components/ListadoPersonajes.jsx
+++ pt-enciclopedia/components/ListadoPersonajes.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ListadoPersonajes = () => {
+
+    const [dataAPI, setDataAPI] = useState()
+
+    useEffect(() => {
+      llamadaApi()
+    }, [])
+    
+
+    const llamadaApi = async () => {
+        try{
+        const call = await fetch(`https://swapi.dev/api/people`);
+        const data = await call.json()
+        // console.log(data.results)
+        setDataAPI(data.results)
+
+        }catch(error){"error detected", error}
+    }
 
     
   return (
